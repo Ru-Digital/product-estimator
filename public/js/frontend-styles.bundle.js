dc866c613c8f89c364923eee030b6bdb47591513
Ru Digital - Ricky Attard <ricky@rudigital.com.au>
2025-05-22 17:39:45 +1000

    Styling

---
+++ public/js/frontend-styles.bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!****************************************!*\
+  !*** ./src/styles/frontend/Index.scss ***!
+  \****************************************/
+// extracted by mini-css-extract-plugin
+/******/ })()
+;
+//# sourceMappingURL=frontend-styles.bundle.js.map
